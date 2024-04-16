6cadd3b7386dee9420fb337c1a8ef4ede66d3b1e
mtahir-cloudkitect <mtahir@cloudkitect.com>
2024-04-15 20:24:03 -0700

    chore: project setup

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,9 +1,20 @@
 import { javascript } from "projen";
 import { monorepo } from "@aws/pdk";
+
 const project = new monorepo.MonorepoTsProject({
   devDeps: ["@aws/pdk"],
   name: "sample-construct",
   packageManager: javascript.NodePackageManager.PNPM,
   projenrcTs: true,
+  buildWorkflow: true,
+  release: true,
+  github: true,
+  githubOptions: {
+    mergify: false
+  },
+  pnpmVersion: "8",
 });
+
+const exclusions = ['.DS_Store', '.idea', '*.iml']
+project.gitignore.exclude(...exclusions)
 project.synth();
