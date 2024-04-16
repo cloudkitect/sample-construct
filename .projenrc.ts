cf16e7ae67cf451534b50ceec2c4b49788a3d3fe
mtahir-cloudkitect <mtahir@cloudkitect.com>
2024-04-15 20:34:55 -0700

    chore: construct module setup

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,5 +1,6 @@
 import { javascript } from "projen";
 import { monorepo } from "@aws/pdk";
+import {AwsCdkConstructLibrary} from "projen/lib/awscdk";
 
 const project = new monorepo.MonorepoTsProject({
   devDeps: ["@aws/pdk"],
@@ -14,6 +15,30 @@ const project = new monorepo.MonorepoTsProject({
   },
   pnpmVersion: "8",
 });
+
+const components = new AwsCdkConstructLibrary({
+  authorAddress: "support@cloudkitect.com",
+  parent: project,
+  defaultReleaseBranch: 'main',
+  cdkVersion: '2.137.0',
+  constructsVersion: '10.3.0',
+  author: 'CloudKitect Inc',
+  authorOrganization: true,
+  repositoryUrl: 'https://github.com/cloudkitect/sample-construct',
+  jsiiVersion: '~5.3.0',
+  name: `@cloudkitect/sample-construct`,
+  outdir: `packages/sample-construct`,
+  packageManager: project.package.packageManager,
+  github: true,
+  release: true,
+  buildWorkflow: true,
+  githubOptions: {
+    mergify: false,
+  },
+  pnpmVersion: "8",
+});
+components.synth()
+
 
 const exclusions = ['.DS_Store', '.idea', '*.iml']
 project.gitignore.exclude(...exclusions)
